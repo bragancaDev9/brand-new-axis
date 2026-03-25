a4229f00fd010860aa5617a128ab29104448fdea
bragancaDev9 <bryanstos99@gmail.com>
2026-03-25 14:27:41 -0300

    Project created

---
+++ src/main/java/com/example/brand_new_axis/BrandNewAxisApplication.java
@@ -0,0 +1,13 @@
+package com.example.brand_new_axis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrandNewAxisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrandNewAxisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brand_new_axis/BrandNewAxisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brand_new_axis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrandNewAxisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
